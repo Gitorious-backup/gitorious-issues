d2a6b3d8735d691aa84aeda20914cbcb3486729a
Piotr Solnica <piotr.solnica@gmail.com>
2013-11-21 22:45:27 +0100

    Tweak issue form

--- app/helpers/issues/application_helper.rb
+++ app/helpers/issues/application_helper.rb
@@ -3,11 +3,9 @@ module Issues
   module ApplicationHelper
 
     def gts_form_for(object, *args, &block)
-      options   = args.extract_options!
-      form_args = args << options.merge(
-        builder: FormBuilder,
-        class:   'form-horizontal'
-      )
+      options = args.extract_options!
+      (options[:html] ||= {}).update(:class => 'form-horizontal')
+      form_args = args << options.merge(builder: FormBuilder)
       form_for(object, *form_args, &block)
     end
   end
